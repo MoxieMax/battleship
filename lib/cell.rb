8ae639017ecde3224a2586a3e9ff19d1f2967f6e
Issa Abdel Mageed <imageed95@gmail.com>
2022-10-19 12:08:40 -0600

    fixed cell error, and added render method. Added testing for render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -7,7 +7,7 @@ class Cell
     @ship = ship
     @fired_upon = false
   end
-  
+
   def empty?
     if @ship == nil
       true
@@ -15,21 +15,30 @@ class Cell
       false
     end
   end
-  
+
   def place_ship(ship_name)
     @ship = ship_name
   end
-  
+
   def fired_upon?
     @fired_upon
   end
-  
+
   def fire_upon
     if @ship != nil
       @fired_upon = true
       ship.hit
     else
-      @fire_upon = true
+      @fired_upon = true
     end
   end
+
+  def render
+    if self.empty? == true && self.fired_upon? == true
+      return "M"
+    elsif self.fired_upon? == false
+      return "."
+    end
+  end
+
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,41 +3,52 @@ require './lib/ship'
 require './lib/cell'
 
 RSpec.describe Cell do
-  let (:cell) {Cell.new("B4")}
+  let (:cell_1) {Cell.new("B4")}
+  let (:cell_2) {Cell.new("C3")}
   let (:cruiser) {Ship.new("Cruiser", 3)}
-  
+
   describe "iteration 1" do
     it "exists" do
-      expect(cell).to be_a(Cell)
+      expect(cell_1).to be_a(Cell)
     end
-    
+
     it "has readable attributes" do
-      expect(cell.coordinate).to eq("B4")
+      expect(cell_1.coordinate).to eq("B4")
     end
-    
+
     it "starts with no ships" do
-      expect(cell.ship).to eq(nil)
-      expect(cell.empty?).to eq(true)
+      expect(cell_1.ship).to eq(nil)
+      expect(cell_1.empty?).to eq(true)
     end
-    
+
     it "can have ship placed on it" do
-      cell.place_ship(cruiser)
-      expect(cell.ship).to eq(cruiser)
-      expect(cell.empty?).to eq(false)
+      cell_1.place_ship(cruiser)
+      expect(cell_1.ship).to eq(cruiser)
+      expect(cell_1.empty?).to eq(false)
     end
-    
+
     it "initializes without having been fired upon" do
-      cell.place_ship(cruiser)
-      expect(cell.ship).to eq(cruiser)
-      expect(cell.fired_upon?).to eq(false)
+      cell_1.place_ship(cruiser)
+      expect(cell_1.ship).to eq(cruiser)
+      expect(cell_1.fired_upon?).to eq(false)
     end
-    
+
     it "recognizes when a cell has been fired upon" do
-      cell.place_ship(cruiser)
-      expect(cell.ship).to eq(cruiser)
-      cell.fire_upon
-      expect(cell.ship.health).to eq(2)
-      expect(cell.fired_upon?).to eq(true)
+      cell_1.place_ship(cruiser)
+      expect(cell_1.ship).to eq(cruiser)
+      cell_1.fire_upon
+      expect(cell_1.ship.health).to eq(2)
+      expect(cell_1.fired_upon?).to eq(true)
+    end
+
+    it 'can render an empty cell' do
+      expect(cell_1.render).to eq(".")
+    end
+
+    it 'can register a miss or a hit' do
+      cell_1.fire_upon
+      expect(cell_1.render).to eq("M")
+
     end
   end
 end
